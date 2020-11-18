f5dc16767a09f1107774af50be5d586a31a1baf3
michele-ghisolfi <62621504+michele-ghisolfi@users.noreply.github.com>
2020-11-18 13:07:30 +0100

    Update 007

--- jQuery/007_FormEControlli/index.js
+++ jQuery/007_FormEControlli/index.js
@@ -3,18 +3,96 @@
 let _form1;
 
 $(document).ready(function () {
-    _form1=$("#form1");
+    _form1 = $("#form1");
 })
 
 function visualizza(codice) {
     let msg = "";
+    let _chk;
+    let _opts;
     switch (codice) {
         case 1:
             msg = _form1.find("input[type=text]:first-of-type").val();
+            break;
+        case 2:
+            // msg = _form1.children("label:nth-of-type(2)").val();
+            // msg = _form1.children("label").eq(1);
+            msg = _form1.children("label").filter(":nth-of-type(2)").children("select").val();
+            break;
+
+        case 3:
+            _chk = _form1.children("fieldset").eq(0).find("input[type=checkbox]");
+            for (let i = 0; i < _chk.length; i++) {
+                msg += _chk.eq(i).prop("name") + " - " + _chk.eq(i).val() + "\n";
+            }
+            break;
+        case 4:
+            _chk = _form1.children("fieldset").eq(0).find("input[type=checkbox]:checked");
+            _chk.each(function (i, ref) {
+                msg += $(ref).prop("name") + " - " + _chk.eq(i).val() + "\n";
+            })
+            break;
+
+        case 5:
+            _chk = _form1.children("fieldset").eq(0).find("input[type=checkbox]").not(":checked");
+            _chk.each(function (i, ref) {
+                msg += $(ref).prop("name") + " - " + _chk.eq(i).val() + "\n";
+            })
+            break;
+
+        case 6:
+            _opts = _form1.children("fieldset:nth-of-type(2)").find("input[type=radio]");
+            if (_opts.is(":checked")) {
+                msg = _opts.filter(":checked").val();
+            } else {
+                msg = "Nessun radio button selezionato";
+            }
+            break;
+
+        case 7:
+            _opts = _form1.children("fieldset:nth-of-type(2)").find("input[type=radio]").not(":checked");
+            _opts.each(function (i, ref) {
+                msg += $(ref).val() + "\n";
+            })
+            break;
+
+        case 8:
+            // let _select = _form1.children("select");
+            let _select = _form1.find("select").last();
+            let _selected = _select.children("option:selected").each(function(i, ref) {
+                msg += $(ref).val() + " - ";
+            })
+            msg += "\n"; // modi differenti per fare stessa cosa
+            for (const item of _select.val()) {
+                msg += item + " - ";    
+            }
+            break;
+
+        default:
+            break;
+    }
+    alert(msg);
+}
+
+function imposta(codice) {
+    switch (codice) {
+        case 1:
+            _form1.find("input[type=text]").first().val("Nuovo valore");
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
             break;
     
         default:
             break;
     }
-    alert(msg);
 }
