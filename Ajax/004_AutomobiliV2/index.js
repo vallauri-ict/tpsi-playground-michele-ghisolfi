8b0f4cf54d6e544a26b5e2f97b527c48718084b6
unknown <micheleghis@gmail.com>
2021-02-06 13:07:04 +0100

    Create ex 004 V2

---
+++ Ajax/004_AutomobiliV2/index.js
@@ -0,0 +1,190 @@
+"use strict"
+
+const URL = "http://localhost:3000"
+let intestazione = [{
+        "tag": "th",
+        "text": "nomeModello",
+        "width": "15%",
+    },
+    {
+        "tag": "th",
+        "text": "colore",
+        "width": "15%",
+    },
+    {
+        "tag": "th",
+        "text": "nomeModello",
+        "width": "15%",
+    },
+    {
+        "tag": "th",
+        "text": "anno",
+        "width": "10%",
+    },
+    {
+        "tag": "th",
+        "text": "img",
+        "width": "20%",
+    },
+    {
+        "tag": "th",
+        "text": "Dettagli",
+        "width": "13%",
+    },
+    {
+        "tag": "th",
+        "text": "Elimina",
+        "width": "12%",
+    }
+]
+
+$(document).ready(function () {
+    let modello_sel
+    let _lstMarche = $("#lstMarche")
+    let _lstModelli = $("#lstModelli")
+    let _table = $("table")
+    let _dettagli = $(".row").eq(2).children("div").eq(1)
+
+    _dettagli.hide()
+
+    let request = inviaRichiesta("get", URL + "/marche")
+    request.fail(errore)
+    request.done(function (marche) {
+        for (const marca of marche) {
+            let op = $("<option>")
+            op.val(marca.id)
+            op.text(marca.nome)
+            op.appendTo(_lstMarche)
+        }
+        _lstMarche.prop("selectedIndex", -1)
+    })
+
+    // ------------------------------------------------
+
+    _lstMarche.on("change", function () {
+        _lstModelli.html("")
+        let codMarca = _lstMarche.val()
+        let request = inviaRichiesta("get", URL + "/modelli?codMarca=" + codMarca)
+        request.fail(errore)
+        request.done(function (modelli) {
+            for (const modello of modelli) {
+                let op = $("<option>")
+                op.val(modello.id)
+                op.text(modello.nome + " - " + modello.alimentazione)
+                op.appendTo(_lstModelli)
+                op.prop("modello",modello)
+            }
+            _lstModelli.prop("selectedIndex", -1)
+        })
+    })
+
+    _lstModelli.on("change", function () {
+        _table.empty()
+        modello_sel = _lstModelli.val()
+
+        let codModello = _lstModelli.val()
+        let request = inviaRichiesta("get", URL + "/automobili?codModello=" + codModello)
+
+        request.fail(errore)
+        request.done(function (automobili) {
+            let thead = $("<thead>")
+            thead.appendTo(_table)
+            let tr = $("<tr>")
+            tr.appendTo(thead)
+            for (let i = 0;i < intestazione.length; i++) {
+                let th = $(`<${intestazione[i].tag}>`)
+                th.appendTo(tr)
+                th.text(intestazione[i].text)
+                th.css("with", intestazione[i].width)
+            }
+            let tbody = $("<tbody>")
+            tbody.appendTo(_table)
+            for (const auto of automobili) {
+                let tr = $("<tr>")
+                tr.appendTo(tbody)
+
+                let td = $("<td>")
+                td.appendTo(tr)
+                td.text((_lstModelli.prop("modello")).nome)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                td.text((_lstModelli.prop("modello")).alimentazione)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                td.text(auto.colore)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                td.text(auto.anno)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                let img = $("<img>")
+                img.appendTo(td)
+                img.prop("src", `img/${auto.img}`)
+                img.css("height", "65px")
+
+                td = $("<td>")
+                td.appendTo(tr)
+                let btn = $("<button>")
+                btn.addClass("btn btn-xs btn-success")
+                btn.appendTo(td)
+                btn.text("Dettagli")
+                btn.prop("automobile", auto)
+                btn.on("click", dettagliClick)
+
+                td = $("<td>")
+                td.appendTo(tr)
+                btn = $("<button>")
+                btn.addClass("btn btn-xs btn-secondary")
+                btn.appendTo(td)
+                btn.prop("id",auto.id)
+                btn.text("Elimina")
+                btn.on("click", eliminaClick)
+            }
+        })
+    })
+    let _salva = $("#btnAggiorna")
+    _salva.on("click", function () {
+        let url = URL + "/automobili/" + $("#txtId").val()
+        let request = inviaRichiesta("patch", url, {"prezzo":parseInt($("#txtPrezzo").val())})
+        request.fail(errore)
+        request.done(function () {
+            alert("record aggiornato correttamente")
+            _lstModelli.trigger("change")
+        })
+    })
+
+    function eliminaClick() {
+        let url = URL+"/automobili/"+$(this).prop("id")
+        let request = inviaRichiesta("delete",url)
+        request.fail(errore)
+        request.done(function () {
+            alert("record eliminato correttamente")
+            _lstModelli.trigger("change")
+        })
+    }
+
+    function dettagliClick() {
+        _dettagli.show()
+        $("#txtId").val(($(this).prop("automobile")).id) 
+        $("#txtTarga").val(($(this).prop("automobile")).targa) 
+        $("#txtColore").val(($(this).prop("automobile")).colore) 
+        $("#txtAnno").val(($(this).prop("automobile")).anno) 
+        $("#txtKm").val(($(this).prop("automobile")).km) 
+        $("#txtPrezzo").val(($(this).prop("automobile")).prezzo) 
+
+        
+        let url = URL + "/modelli/" + modello_sel
+        let request = inviaRichiesta("get", url)
+        request.fail(errore)
+        request.done(function (modello) {
+            $("#txtNome").val(modello.nome) 
+            $("#txtAlimentazione").val(modello.alimentazione) 
+            $("#txtCilindrata").val(modello.cilindrata) 
+        })
+    }
+
+})
