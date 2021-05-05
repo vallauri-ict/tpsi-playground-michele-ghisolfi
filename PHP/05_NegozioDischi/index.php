ea97d8cc00db43a791e06eae6ada96f1326fc88d
unknown <micheleghis@gmail.com>
2021-05-05 11:07:06 +0200

    Create 05 - TODO

---
+++ PHP/05_NegozioDischi/index.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE html> 
+<html>
+	<head lang="it">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Dischi</title>
+		<!-- BOOTSTRAP e JQUERY -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+		<!-- SPECIFICI -->
+		<link rel="stylesheet" href="index.css">
+		<script src="index.js"></script>
+	</head>
+	<body>
+		
+			<h1 class='text-center' style="margin:20px">Vendita dischi</h1>
+			<div id="table">
+				<form class='table-info text-center' style="padding:15px">
+					<div><b>Codice</b></div>
+					<div><b>Autore</b></div>
+					<div><b>Titolo</b></div>
+					<div><b>Anno</b></div>
+					<div><b>Salva</b></div>
+					<div><b>Cancella</b></div>
+				</form>
+			</div>
+			 
+			<br>
+			<center>
+				<button type='button'
+						class='btn btn-primary'
+						onclick='window.location.href="inserisci.html"'>Aggiungi</button>
+			</center>
+		</div>
+	</body>
+</html>
